992e102b7c72cf2e6a7ba90dead4604ec368b037
Neto Aparovich <aparovich.gregory@gmail.com>
2023-06-07 18:23:09 +0300

    add new name

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 13676;
-        int theNumberOfRublesForOneBonusMile = 20;
+        int rublesPerMile = 20;
 
-        int bonus = amount / theNumberOfRublesForOneBonusMile;
+        int bonus = amount / rublesPerMile;
         System.out.println("Начисленно " + bonus + " бонусов");
     }
 }
